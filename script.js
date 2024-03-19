e07b3687ae98dc287b5684329a52afe3770d8ef7
Ratnamanjari Mahala <162942871+ratnagithub03@users.noreply.github.com>
2024-03-19 13:41:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const canvas = document.getElementById('canvas');
+const increasBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+const ctx = canvas.getContext('2d');
+
+let size = 10
+let isPressed =false
+let color = 'black'
+let x
+let y
+
+canvas.addEventListener('mousedown', (e) => {
+isPressed = true
+
+
+x = e.offsetX
+x = e.offsetY
+
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+    
+    
+    x = undefined
+    x = undefined
+    
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = y2
+        y = y2
+    }
+
+    
+    
+
+})    
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2 ) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.LineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.linewidth = size * 2
+    ctx.stroke()
+
+
+}
+function updateSizeOnScreen(){
+    sizeEl.innerText = size
+}
+
+
+increasBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50
+    }
+    updateSizeOnScreen()
+
+})  
+
+    decreaseBtn.addEventListener('click', () => {
+        size -= 5
+    
+        if(size < 5) {
+            size = 5
+        }
+
+ updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color =e.target.value)
+
+clearEl.addEventListener('click',() => ctx.clearRect(0,0, canvas.clientWidth,canvas.height))
+
